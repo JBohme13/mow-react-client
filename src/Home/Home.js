c14a12f132aa50e69845f6a90bfebda70498a452
Jordan Bohme <jordan.m.bohme@gmail.com>
2019-10-27 15:47:06 -0600

    updated /home

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react'
+import './Home.css'
 
 export default class Home extends Component {
     render() {
         return(
-            <div>*current meal schedule/ prompt to create one here*</div>
+            <section id='home-container'>
+                <h1 id='home-banner'>Click th Meal Schedule tab above to get started</h1>
+                <h2 id='home-sub-banner'>your meal schedule will display here once you have set it up.</h2>
+            </section>
         )
     }
 }
